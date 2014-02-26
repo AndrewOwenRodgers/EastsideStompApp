5106f74aebb7b12761038eecc6d1c60f8167bf57
AndrewOwenWaldo <owenrhyfelwr@gmail.com>
2014-02-26 12:41:29 -0800

    Initial Commit

---
+++ EastsideStompApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  EastsideStompApp
+//
+//  Created by Andrew Rodgers on 2/26/14.
+//  Copyright (c) 2014 Andrew Rodgers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ EastsideStompApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  EastsideStompApp
+//
+//  Created by Andrew Rodgers on 2/26/14.
+//  Copyright (c) 2014 Andrew Rodgers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
