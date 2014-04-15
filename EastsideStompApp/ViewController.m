e46a55d2f82facec5baef5e9e0a5810d381623ff
AndrewOwenWaldo <owenrhyfelwr@gmail.com>
2014-04-14 19:21:49 -0700

    Started building HTTP requests

--- EastsideStompApp/ViewController.m
+++ EastsideStompApp/ViewController.m
@@ -166,4 +166,26 @@
     return twitterCell;
 }
 
+#pragma mark -HTTP requests
+
+-(NSDictionary *)httpRequestWithURL:(NSURL*)url
+{
+	NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
+    [request setHTTPMethod:@"GET"];
+    [request setURL:[NSURL URLWithString:url]];
+	
+    NSError *error = [[NSError alloc] init];
+    NSHTTPURLResponse *responseCode = nil;
+	
+    NSData *oResponseData = [NSURLConnection sendSynchronousRequest:request returningResponse:&responseCode error:&error];
+	
+    if([responseCode statusCode] != 200)
+	{
+        NSLog(@"Error getting %@, HTTP status code %li", url, (long)[responseCode statusCode]);
+        return nil;
+    }
+	
+	return [[NSString alloc] initWithData:oResponseData encoding:NSUTF8StringEncoding];
+}
+
 @end
